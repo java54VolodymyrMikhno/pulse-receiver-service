2aa8452c9cec1f1b4493854c7779e1aebae63b8f
VolodymyrMikhno <volodymyr3583@gmail.com>
2024-10-26 22:21:48 +0300

    removed unused code

--- src/main/java/telran/monitoring/pulse/PulseReceiverAppl.java
+++ src/main/java/telran/monitoring/pulse/PulseReceiverAppl.java
@@ -1,28 +1,25 @@
 package telran.monitoring.pulse;
 import java.net.*;
 import java.util.Arrays;
-
-import org.json.JSONObject;
 public class PulseReceiverAppl {
 private static final int PORT = 5000;
 private static final int MAX_BUFFER_SIZE = 1500;
 static DatagramSocket socket;
-	public static void main(String[] args) throws Exception {
-		socket = new DatagramSocket(PORT);
+	public static void main(String[] args) throws Exception{
+		socket  = new DatagramSocket(PORT);
 		byte [] buffer = new byte[MAX_BUFFER_SIZE];
 		while(true) {
-			DatagramPacket packet = new DatagramPacket(buffer, PORT);
+			DatagramPacket packet = new DatagramPacket(buffer, MAX_BUFFER_SIZE);
 			socket.receive(packet);
-			processReceivedData(buffer,packet);
+			processReceivedData(buffer, packet);
 		}
+
 	}
-	private static void processReceivedData(byte[] buffer, DatagramPacket packet) {
+	private static void processReceivedData(byte[] buffer,
+			DatagramPacket packet) {
 		String json = new String(Arrays.copyOf(buffer, packet.getLength()));
-		JSONObject js = new JSONObject(json);
-		long pat =js.getLong("patientId");
-		if (pat ==1){
-			System.out.println(json);
-		}
+		System.out.println(json);
+		
 		
 	}
 
